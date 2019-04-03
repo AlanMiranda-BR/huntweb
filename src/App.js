03de01cf26928d93b5a90a0ed516a260d5bca31f
Gabriel Pagelkopf <gabriel.pagelkopf@gmail.com>
2019-04-02 21:33:26 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import './styles.css';
+import Header from './Components/Header';
+
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+);
+
+export default App;
